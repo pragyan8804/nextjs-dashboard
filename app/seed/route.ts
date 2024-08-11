13aee63953de2c37077873864065f3b07347829d
pragyan8804 <89921612+pragyan8804@users.noreply.github.com>
2024-08-11 12:40:40 +0530

    changes

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -100,7 +100,12 @@ async function seedRevenue() {
 
   return insertedRevenue;
 }
- 
+
+export async function GET() {
+  return Response.json({
+    message:
+      'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  });
   try {
     await client.sql`BEGIN`;
     await seedUsers();
